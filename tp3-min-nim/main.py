434206cf115a4b2b6a2ef8030eb2b240be720188
Manuel Panichelli <panicmanu@gmail.com>
2023-07-18 21:23:13 -0300

    jugada otima

--- tp3-min-nim/main.py
+++ tp3-min-nim/main.py
@@ -20,7 +20,7 @@ def calculate_positions(n1, n2, n3, r1, r2, r3):
 
     #print(f"p positions: {p_pos}\nn positions: {n_pos}")
     play_interactive_game(
-        playerL=HumanPlayer(),
+        playerL=OptimalComputerPlayer(),
         playerR=HumanPlayer(),
         game=initial_game,
     )
@@ -99,7 +99,7 @@ class Simulator:
     Simula todas las partidas posibles para obtener los conjuntos de
     P-posiciones y N-posiciones.
     """
-    outcomes: Dict[str, Tuple[Position, OutcomeClass]]
+    outcomes: Dict[str, Tuple[Position, OutcomeClass, float]]
 
     def calculate_positions_for_game(
             self, 
@@ -126,60 +126,70 @@ class Simulator:
             print(f"{indent}outcome memoized for {game.position}: {outcome_to_str(outcome)}")
             return outcome
     
-        outcome = self._outcome(game, indent)
-        self._store_outcome(game, outcome)
+        outcome, value = self._outcome(game, indent)
+        self._store_outcome(game, outcome, value)
 
         return outcome
 
-    def _outcome(self, game: Game, indent: str) -> OutcomeClass:
+    def _outcome(self, game: Game, indent: str) -> Tuple[OutcomeClass, float]:
+
+        # TODO: Poda: si el min move no se puede hacer, cortas guardarlos en
+        # orden y que el min sea el primero pedirle los legal moves al juego
         outcomes = []
         print(f"{indent}{game.position}")
         for heap_idx in game.positions():
-            # TODO: Poda: si el min move no se puede hacer, cortas
-            # guardarlos en orden y que el min sea el primero
-            # pedirle los legal moves al juego
             for move in game.legal_moves(heap_idx):
                 game_result = game.move(heap_idx, move)
+
                 print(f"{indent}\ttake {move} from {game.heap(heap_idx)} (#{heap_idx})")
+
                 sub_outcome = self._mem_outcome(game_result, indent+"\t\t")
                 outcomes.append(sub_outcome)
                 
                 print(f"{indent}\t-> next player outcome {outcome_to_str(sub_outcome)}")
 
-        game_outcome = self._outcome_from_outcomes(outcomes)
-        print(f"{indent}next player outcomes: {outcomes_to_str(outcomes)} -> {outcome_to_str(game_outcome)}")
+        game_outcome, game_value = self._outcome_from_outcomes(outcomes)
+        print(f"{indent}next player outcomes: {outcomes_to_str(outcomes)} -> {outcome_to_str(game_outcome)}, val {game_value}")
 
-        return game_outcome
+        return game_outcome, game_value
 
-    def _outcome_from_outcomes(self, outcomes: List[OutcomeClass]) -> OutcomeClass:
+    def _outcome_from_outcomes(self, outcomes: List[OutcomeClass]) -> Tuple[OutcomeClass, float]:
         """
         Calcula el outcome class de una posición en base a los outcome
         classes de las posiciones resultantes de hacer todos los movimientos
-        legales
+        legales.
+        Además, calcula el valor de la posición como la proporción de jugadas ganadoras.
         """
         if len(outcomes) == 0:
             # Caso base: No tengo movimientos, pierdo
-            return P
+            return P, 0
 
         # Si tengo al menos una subclase P, quiere decir que puedo forzar
         # un movimiento ganador, por lo que soy N
         p_outcomes = list(filter(lambda s: s == P, outcomes))
         if len(p_outcomes) != 0:
-            return N
+            return N, len(p_outcomes) / len(outcomes)
         
         # Si todas las subclases son N (no hay P) sin importar que haga pierdo.
         # Soy P
-        return P
+        return P, 0
 
-    def _store_outcome(self, game: Game, outcome: OutcomeClass):
-        self.outcomes[self._key(game)] = (game.position, outcome)
+    def _store_outcome(self, game: Game, outcome: OutcomeClass, value: float):
+        self.outcomes[self._key(game)] = (game.position, outcome, value)
 
     def _find_outcome(self, game: Game) -> OutcomeClass:
         if self._key(game) not in self.outcomes:
             return None
     
-        _, outcome = self.outcomes[self._key(game)]
+        _, outcome, _ = self.outcomes[self._key(game)]
         return outcome
+
+    def find_value(self, game: Game) -> float:
+        if self._key(game) not in self.outcomes:
+            return None
+        
+        _, _, value = self.outcomes[self._key(game)]
+        return value
 
     def _key(self, game: Game) -> str:
         """
@@ -190,24 +200,48 @@ class Simulator:
         return ''.join(map(str, sorted(game.position)))
 
 
+@dataclass
+class PlayerMove:
+    heap_idx: int
+    take: int
 
 class Player:
     """Jugador de min nim"""
-    def choose_move(self, game: Game) -> Tuple[int, int]:
+    def choose_move(self, game: Game) -> PlayerMove:
         raise NotImplementedError
 
 class HumanPlayer(Player):
-    def choose_move(self, game: Game) -> Tuple[int, int]:
+    def choose_move(self, game: Game) -> PlayerMove:
         print(f"Estado del juego: {game.position}")
         positions = list(map(lambda p: p+1, game.positions()))
         heap = input(f"pila # ({positions}): ")
         take = input(f"tomar ({game.moves}): ")
 
-        return int(heap) - 1, int(take)
+        return PlayerMove(
+            heap_idx=int(heap) - 1,
+            take=int(take),
+        )
 
 class OptimalComputerPlayer(Player):
-    def choose_move(self, game: Game) -> Tuple[int, int]:
-        pass
+    def __init__(self):
+        self.simulator = Simulator(outcomes={})
+
+    def choose_move(self, game: Game) -> PlayerMove:
+        if self.simulator._find_outcome(game) is None:
+            self.simulator._mem_outcome(game)
+    
+        # (value, move)
+        values: List[Tuple[float, PlayerMove]] = []
+
+        for heap_idx in game.positions():
+            for move in game.legal_moves(heap_idx):
+                game_result = game.move(heap_idx, move)
+                val = self.simulator.find_value(game_result)
+
+                values.append((val, PlayerMove(heap_idx=heap_idx, take=move)))
+
+        # return argmin values
+        return min(values, key=lambda t: t[0])[1]
 
 def play_interactive_game(
     playerL: Player,
@@ -218,21 +252,23 @@ def play_interactive_game(
     playerR: Tuple[str, Player] = ("R", playerR)
 
     player = playerL
-    nextPlayer = playerR
+    next_player = playerR
     while not game.finished():
         print(f"Turno de {player[0]}")
-        heap, take = player[1].choose_move(game)
+        move = player[1].choose_move(game)
 
-        while not game.is_legal_move(heap, take):
+        while not game.is_legal_move(move.heap_idx, move.take):
             print("Movimiento ilegal, elegí de vuelta")
-            heap, take = player[1].choose_move(game)
+            move = player[1].choose_move(game)
+        
+        print("Movimiento elegido")
 
-        game = game.move(heap, take)
+        game = game.move(move.heap_idx, move.take)
 
-        player, nextPlayer = nextPlayer, player
+        player, next_player = next_player, player
         print("-" * 20)
     
-    print(f"Gana {nextPlayer[0]}!")
+    print(f"Gana {next_player[0]}!")
 
 def parse_args(args: List[str]) -> Tuple[int, int, int, int, int, int]:
     if not (len(args) == 7):
